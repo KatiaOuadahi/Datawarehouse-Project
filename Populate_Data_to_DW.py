eae3ee398e7324dd820dd337707f08922893564b
Maroua Bouchareb <boucharebmaroua3@gmail.com>
2024-05-17 01:40:27 +0100

    add generate_DateDWID function

--- Populate_Data_to_DW.py
+++ Populate_Data_to_DW.py
@@ -3,13 +3,22 @@ import pandas as pd
 import datetime
 
 
-# Convertir la date complaite de format 'YYYY-MM-DD' à 'YYYYMMDD'
+# Fonction pour convertir la date compléte de format 'YYYY-MM-DD' à 'YYYYMMDD'
 def get_id_date(fulldate):
     return datetime.datetime.strptime(fulldate, '%Y-%m-%d').strftime('%Y%m%d')
   
     
   
+# Fonction qui combinne la date et le code du sation 
+# pour génénrer un dateid unique
+def generate_DateDWID(fulldate, sationcode):
+    date_part = get_id_date(fulldate)
+    return f"{date_part}{sationcode}"  
+
+
+
 # Fonction pour peupler la table Date_Dim
+
 def populate_Date_Dim(cursor, csvpath):
     
     # Lire le fichier CSV en utilisant uniquement les colonnes nécessaires
@@ -22,15 +31,20 @@ def populate_Date_Dim(cursor, csvpath):
    
                                                         
     for _, row in Date_Dim.iterrows():
-        Date = get_id_date(row['Full_date']) 
+        # Convertir la date compléte pour quelle s'adapte
+        # avec le type de l'attribut "DateDWID" de la table 'Date_Dim'
+        FullDate = get_id_date(row['Full_date'])
         
-        query_insert = "INSERT INTO Date_Dim ( Full_Date, Day_Name, WeekendFlag," \
-                                           " Month_Name, Month_Number,"\
-                                           " Year_And_Quarter, Year_And_Semester, " \
-                                           " Season, Year)" \
-                                   "VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s) "
+        # Utilise la fonction generate_DateDWID pour génerer la clé primaire de la table 'Date_Dim'
+        DATEID = generate_DateDWID(row['Full_date'],row['STATION'])
+        
+        query_insert = "INSERT INTO Date_Dim ( DateDWID, Full_Date, Day_Name, WeekendFlag," \
+                                             " Month_Name, Month_Number,"\
+                                             " Year_And_Quarter, Year_And_Semester, " \
+                                             " Season, Year)" \
+                                     "VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) "
                                    
-        values = ( Date, row['Day_Name'], row['WeekendFlag'],
+        values = ( DATEID, FullDate, row['Day_Name'], row['WeekendFlag'],
                    row['Month_Name'], row['Month'], row['quarter'],
                    row['semester'], row['Season'], row['year'])
                          
@@ -49,7 +63,7 @@ def populate_Station_Dim(cursor, csv_path):
 
     for _, row in Station_Dim.iterrows():
         query_insert = "INSERT INTO Station_Dim ( StationID , NAME , COUNTRY_NAME , LATITUDE , LONGITUDE , ELEVATION)"\
-                                      "VALUES ( %s, %s, %s, %s, %s, %s ) "
+                                        "VALUES ( %s, %s, %s, %s, %s, %s ) "
         
         values = (row['STATION'], row['NAME'],
                   row['country_name'] ,row['LATITUDE'],
@@ -62,25 +76,24 @@ def populate_Station_Dim(cursor, csv_path):
 ##########################################################################################
 
 # Fonction pour peupler la table fact_Weather
-def populate_Weather_fact(cursor, csv_path): 
+def populate_Weather_Fact(cursor, csv_path): 
     # Lire le fichier CSV en utilisant uniquement les colonnes nécessaires
-    FactWeather = pd.read_csv(csv_path, usecols=['Full_date','PRCP', 'SNOW', 'SNWD',
-                                                 'TAVG', 'TMIN', 'TMAX',
-                                                 'PGTM', 'WSFG','WDFG'])
+    FactWeather = pd.read_csv(csv_path, usecols = ['STATION','Full_date','PRCP', 'SNOW', 'SNWD',
+                                                   'TAVG', 'TMIN', 'TMAX',
+                                                   'PGTM', 'WSFG','WDFG'])
     
     # Remplacer les valeurs NaN par 'N/A' dans le DataFrame FactWeather
     FactWeather.fillna('N/A', inplace=True)
         
     
     for _, row in FactWeather.iterrows():
-        # Convertir la date complète en format YYYYMMDD 
-        # pour être inserer dans la colonne DateDWID
-        Date = get_id_date(row['Full_date']) 
+        DATEID = generate_DateDWID(row['Full_date'],row['STATION'])
+
 
         query_insert = "INSERT INTO Weather_Fact ( DateDWID , PRCP, SNOW , SNWD ,TAVG, TMIN, TMAX, PGTM, WSFG , WDFG ) "\
-                                       "VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) "
+                                         "VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) "
         
-        values = (Date, row['PRCP'], row['SNOW'], row['SNWD'],
+        values = (DATEID, row['PRCP'], row['SNOW'], row['SNWD'],
                   row['TAVG'], row['TMIN'], row['TMAX'],
                   row['PGTM'], row['WSFG'] , row['WDFG'])
         
@@ -90,7 +103,7 @@ def populate_Weather_fact(cursor, csv_path):
         
         
         
-# Connecte to the DataBase
+# Connecter à la BDD 'WeatherDataWarehouse'
 connection = pymysql.connect(host='localhost',
                                user='root',
                                password='',
@@ -100,19 +113,21 @@ connection = pymysql.connect(host='localhost',
 
 cursor = connection.cursor()
 
+print("Start populating 'Date_Dim' table" )
 populate_Date_Dim(cursor,"climatic_dataSet.csv")
-print("Date Dim populated")
+print("'Date_Dim' populated")
 
+print("\nStart populating 'Station_Dim' table" )
 populate_Station_Dim(cursor,"climatic_dataSet.csv")
-print("Station Dim populated")
+print("'Station_Dim' populated")
+
+print("\nStart populating 'Weather_Fact' table" )
+populate_Weather_Fact(cursor,"climatic_dataSet.csv")
+print("'Weather_Fact' populated")
 
 
-populate_Weather_fact(cursor,"climatic_dataSet.csv")
-print("Weather Fact populated")
 
-
-
-# Close the connection
+# Fermer la connexion
 cursor.close()
 connection.commit()
 connection.close()
